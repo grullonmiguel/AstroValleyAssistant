efe2aed79a6adbee1bb380490a51e54f27e59597
mgrullon <grullonmiguel@gmail.com>
2025-10-04 15:09:24 -0400

    Rename MainWindow to MainView, add ViewModelBase, and create MainViewModel skeleton

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AstroValleyAssistant.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroValleyAssistant.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+    }
+}
