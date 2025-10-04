9da25d5ae5fe628cc192b918720e460d78cee452
mgrullon <grullonmiguel@gmail.com>
2025-10-04 15:51:35 -0400

    Implement minimal MainView UI skeleton for search and results display

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,13 +1,36 @@
 ﻿using AstroValleyAssistant.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AstroValleyAssistant.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => Set(ref _currentViewModel, value);
+        }
+        private ViewModelBase _currentViewModel;
+
+        #region Methods
+
+        private void ExecuteNavigate(object parameter)
+        {
+            //string viewName = parameter as string;
+            //switch (viewName)
+            //{
+            //    case "Regrid":
+            //        CurrentViewModel = _serviceProvider.GetRequiredService<RegridViewModel>();
+            //        break;
+            //    case "RealAuction":
+            //        CurrentViewModel = _serviceProvider.GetRequiredService<RealAuctionViewModel>();
+            //        break;
+            //        // etc.
+            //}
+        }
+
+        #endregion
     }
 }
