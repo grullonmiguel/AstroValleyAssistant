0b0d98897098d590766b4b1f5af791b7b04dc63a
mgrullon <grullonmiguel@gmail.com>
2026-01-19 19:44:17 -0500

    Adds json file for RealAuction URLs

---
+++ Models/AuctionRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace AstroValleyAssistant.Models
+{
+    public record AuctionRecord(
+        string ParcelId,
+        string PropertyAddress,
+        decimal OpeningBid,
+        decimal? AssessedValue,
+        DateTime AuctionDate,
+        int PageNumber,
+        string AppraiserUrl
+    );
+}
